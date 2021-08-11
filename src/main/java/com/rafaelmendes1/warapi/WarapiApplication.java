508f64889c6318d83b0f5446582332e0b1f9fed4
rafaelmendes1 <rarafafa3245@gmail.com>
2021-08-11 00:11:54 -0300

    project created

---
+++ src/main/java/com/rafaelmendes1/warapi/WarapiApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelmendes1.warapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelmendes1/warapi/WarapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmendes1.warapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
